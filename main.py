1d7c89e219c8c8658c257f540e8831e24f6e467f
glowcouch <42631203+glowcouch@users.noreply.github.com>
2022-02-26 16:09:27 +1300

    fix drawing screen

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ playerY = 0
 
 def renderScreen():
     os.system("clear")
-    for y in range(0 - (int(20 / 2)), int(20 / 2)):
-        for x in range(0 - (20 / 2), 20 / 2):
+    for y in range(0, worldHeight):
+        for x in range(0, worldWidth):
             if x == playerX and y == playerY:
                 print(":)", end="")
             else:
                 print("00", end="")
         print("")
 
-
+renderScreen()
 while True:
     inp = input()
 
@@ -31,12 +31,12 @@ while True:
     if inp == "d":
         playerX += 1
 
-    if playerX > 10:
-        playerX = 10
-    if playerX < -10:
-        playerX = -10
-    if playerY < -10:
-        playerY = -10
-    if playerY > 10:
-        playerY = 10
+    if playerX > worldWidth:
+        playerX = worldWidth
+    if playerX < 0:
+        playerX = 0
+    if playerY < 0:
+        playerY = 0
+    if playerY > worldHeight:
+        playerY = worldHeight
     renderScreen()
